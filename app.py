64c61c9c7bef88543fd5e7be6fb6bb783e388587
yasuokeyblade <wj922103@gmail.com>
2024-09-18 23:43:30 +0000

    alteracao

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
+import pika
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
-from flask_cors import CORS  # Importa o CORS
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS no Flask
+CORS(app)
+
 app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app, cors_allowed_origins="*")  # Permite todas as origens
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Função para enviar mensagem para o RabbitMQ
+def send_to_rabbitmq(message):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('0.0.0.0'))
+    channel = connection.channel()
+    channel.queue_declare(queue='chatQueue')
+    channel.basic_publish(exchange='', routing_key='chatQueue', body=message)
+    connection.close()
+
+# Função para consumir mensagem do RabbitMQ
+def consume_from_rabbitmq():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('0.0.0.0'))
+    channel = connection.channel()
+    channel.queue_declare(queue='chatQueue')
+
+    def callback(ch, method, properties, body):
+        # Emitir a mensagem para todos os clientes conectados via WebSocket
+        socketio.emit('message', body.decode())  # Removi o argumento 'broadcast=True'
+
+    channel.basic_consume(queue='chatQueue', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
 
 @app.route('/')
 def index():
@@ -14,7 +37,10 @@ def index():
 @socketio.on('message')
 def handleMessage(msg):
     print('Mensagem recebida: ' + msg)
-    send(msg, broadcast=True)
+    # Envia a mensagem para o RabbitMQ
+    send_to_rabbitmq(msg)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    # Executa o consumidor RabbitMQ em uma thread separada
+    socketio.start_background_task(target=consume_from_rabbitmq)
+    socketio.run(app, host='0.0.0.0', port=5000)
